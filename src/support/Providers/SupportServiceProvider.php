60595ce0b401f0ef93e9f9f14f0ce8c4a08ff554
omadonex <omadonex@yandex.ru>
2022-12-29 12:52:03 +0300

    wip

--- src/support/Providers/SupportServiceProvider.php
+++ src/support/Providers/SupportServiceProvider.php
@@ -19,38 +19,38 @@ class SupportServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $pathRoot = realpath(__DIR__.'/../..');
+        $pathRoot = realpath(__DIR__.'/../../..');
 
-        $this->loadMigrationsFrom("{$pathRoot}/database/migrations");
+        //$this->loadMigrationsFrom("{$pathRoot}/database/migrations");
         //$this->loadViewsFrom("{$pathRoot}/resources/views", 'support');
         $this->loadViewsFrom("{$pathRoot}/resources/views/modal", 'omx-modal');
-        $this->loadTranslationsFrom("{$pathRoot}/resources/lang", 'support');
-
-        $this->publishes([
-            "{$pathRoot}/config/modules.php" => config_path('modules.php'),
-        ], 'config');
-        $this->publishes([
-            "{$pathRoot}/resources/views" => resource_path('views/vendor/support'),
-        ], 'views');
-        $this->publishes([
-            "{$pathRoot}/resources/lang" => resource_path('lang/vendor/support'),
-        ], 'translations');
-
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                Make::class,
-                Remove::class,
-                MakeModel::class,
-                RemoveModel::class,
-            ]);
-        }
-
-        $this->commands([
-            UnsafeSeeding::class,
-        ]);
-
-        Validator::extend('time', 'Omadonex\LaravelTools\Support\Services\CustomValidator@timeValidate');
-        Validator::extend('phone', 'Omadonex\LaravelTools\Support\Services\CustomValidator@phoneValidate');
+//        $this->loadTranslationsFrom("{$pathRoot}/resources/lang", 'support');
+//
+//        $this->publishes([
+//            "{$pathRoot}/config/modules.php" => config_path('modules.php'),
+//        ], 'config');
+//        $this->publishes([
+//            "{$pathRoot}/resources/views" => resource_path('views/vendor/support'),
+//        ], 'views');
+//        $this->publishes([
+//            "{$pathRoot}/resources/lang" => resource_path('lang/vendor/support'),
+//        ], 'translations');
+//
+//        if ($this->app->runningInConsole()) {
+//            $this->commands([
+//                Make::class,
+//                Remove::class,
+//                MakeModel::class,
+//                RemoveModel::class,
+//            ]);
+//        }
+//
+//        $this->commands([
+//            UnsafeSeeding::class,
+//        ]);
+//
+//        Validator::extend('time', 'Omadonex\LaravelTools\Support\Services\CustomValidator@timeValidate');
+//        Validator::extend('phone', 'Omadonex\LaravelTools\Support\Services\CustomValidator@phoneValidate');
     }
 
     /**
