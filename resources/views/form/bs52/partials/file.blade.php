87eedc20363b0e5ff6982a073f2d95eb21dfaed5
goray <goraynov@mail.ru>
2023-01-25 00:30:47 +0300

    task_68#user_profile: users profile

--- resources/views/form/bs52/partials/file.blade.php
+++ resources/views/form/bs52/partials/file.blade.php
@@ -1,1 +1,6 @@
-<input type="file" onchange="{{ $onchange }}" id="{{ $id }}" name="{{ $name }}" class="{{ $class }}">
+<input type="file" id="{{ $id }}" name="{{ $name }}"
+       class="form-control {{ $class }} {{ count($errors->get($name)) ? 'is-invalid' : '' }}"
+       @if (isset($placeholder)) placeholder="{{ $placeholder }}" @endif
+       data-jst-field="{{ $name }}"
+>
+<div data-jst-field="{{ $name }}" class="invalid-feedback">{{ $errors->first($name) }}</div>
