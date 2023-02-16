8443822897e49507bb22e8a21bfab05fc4123f3b
vlados <vlad.os.011235@gmail.com>
2023-02-16 12:10:22 +0300

    checkbox

--- resources/views/form/bs52/partials/checkbox.blade.php
+++ resources/views/form/bs52/partials/checkbox.blade.php
@@ -1,5 +1,6 @@
 <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="{{ $id }}" name="{{ $name }}"
+    <input type="checkbox" class="form-check-input {{ $class ?? '' }}" id="{{ $id }}" name="{{ $name }}"
+           @if(isset($checked) && $checked === true) checked @endif
            data-jst-field="{{ $name }}" @isset($noValidate) data-jst-no-validate="true" @else @isset($validate) data-jst-validate="{{ $validate }}" @endisset @endisset>
     <label class="form-check-label" for="{{ $id }}">{{ $label }}</label>
 </div>
