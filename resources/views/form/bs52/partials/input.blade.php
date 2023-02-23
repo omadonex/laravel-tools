31599216345477f9fcd165e6929def491d57f3b7
vlados <vlad.os.011235@gmail.com>
2023-02-23 11:42:34 +0300

    fix

--- resources/views/form/bs52/partials/input.blade.php
+++ resources/views/form/bs52/partials/input.blade.php
@@ -1,7 +1,7 @@
 @if (!isset($noLabel) || !$noLabel)
     <label for="{{ $id }}" class="form-label">{{ $label }}</label>
 @endif
-<input type="text" id="{{ $id }}" name="{{ $name }}" class="form-control {{ count($errors->get($name)) ? 'is-invalid' : '' }}" placeholder="{{ $placeholder }}"
+<input type="text" id="{{ $id }}" name="{{ $name }}" class="form-control {{ $class ?? '' }} {{ count($errors->get($name)) ? 'is-invalid' : '' }}" placeholder="{{ $placeholder }}"
        @if (old($name)) value="{{ old($name) }}" @elseif(isset($value)) value="{{ $value }}" @endif
        data-jst-field="{{ $name }}" @isset($noValidate) data-jst-no-validate="true" @else @isset($validate) data-jst-validate="{{ $validate }}" @endisset @endisset>
 <div data-jst-field="{{ $name }}" class="invalid-feedback">{{ $errors->first($name) }}</div>
--- resources/views/form/bs52/partials/submit.blade.php
+++ resources/views/form/bs52/partials/submit.blade.php
@@ -1,4 +1,4 @@
-<button data-jst-submit id="{{ $formId }}__btnSubmit" type="button" class="btn btn-primary mt-6 mb-2 @isset($submitClass) {{ $submitClass }} @endif">
+<button data-jst-submit id="{{ $formId }}__btnSubmit" type="button" style="{{ $style ?? '' }}" class="btn btn-primary mt-6 mb-2 @isset($submitClass) {{ $submitClass }} @endif">
     <span data-jst-spinner style="margin-top: 0.15rem;" id="{{ $formId }}__spnSubmitSpinner" class="spinner-grow spinner-grow-sm float-end d-none" role="status" aria-hidden="true"></span>
     {{ $text }}
 </button>
