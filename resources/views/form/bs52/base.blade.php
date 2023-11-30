2e058cc5ff85546d9edfb0cb937546306161f8c3
omadonex <omadonex@yandex.ru>
2023-11-30 05:04:49 +0300

    wip

--- resources/views/form/bs52/base.blade.php
+++ resources/views/form/bs52/base.blade.php
@@ -1,5 +1,8 @@
-<form id="{{ $formId }}" method="{{ $method }}" action="{{ $action }}" @if(isset($enctype)) enctype="{{ $enctype }}" @endif class="needs-validation" novalidate>
+<form id="{{ $formId }}" method="{{ $method == 'GET' ? $method : 'POST' }}" action="{{ $action }}" @if(isset($enctype)) enctype="{{ $enctype }}" @endif class="needs-validation" novalidate>
     @csrf
+    @if (!in_array($method, ['GET', 'POST']))
+        @method($method)
+    @endif
     @include('omx-form::bs52.partials.alert')
     @yield("{$formId}-body")
 </form>
