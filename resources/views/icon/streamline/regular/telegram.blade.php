805d159206f34ee06c16d4216a97e5230ad74788
omadonex <omadonex@yandex.ru>
2024-10-15 04:49:12 +0300

    wip

---
+++ resources/views/icon/streamline/regular/telegram.blade.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="{{ $class }}" width='{{ $width }}' height='{{ $height }}'>
+    <circle cx="12" cy="12" r="11.25" fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="{{ $stroke }}0"/>
+    <path fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15.005,17.743,17.438,7.5a.5.5,0,0,0-.664-.583l-12.5,4.766L9.75,13.25Z" stroke="{{ $stroke }}0"/>
+    <polyline fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" points="8.094 12.776 8.75 17.5 12.182 15.33" stroke="{{ $stroke }}0"/>
+    <line x1="9.75" x2="13.25" y1="13.25" y2="11" fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="{{ $stroke }}0"/>
+</svg>
