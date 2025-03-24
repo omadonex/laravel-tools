d5f221fa76a7827ab3710e2d152de2feaa340a31
omadonex <omadonex@yandex.ru>
2025-03-24 21:03:35 +0300

    wip

---
+++ src/support/Commands/ModelBootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Omadonex\LaravelTools\Support\Commands;
+
+use Illuminate\Console\Command;
+
+class ModelBootstrap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'omx:support:model-bootstrap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate all files for bootstraping model';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+}
