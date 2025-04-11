b12a48e5755baec4e7804a8782b30e83d32f0f5c
omadonex <omadonex@yandex.ru>
2025-04-11 13:49:36 +0300

    wip

---
+++ resources/views/icon/streamline/regular/reward-stars-2.blade.php
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="{{ $class }}" width="{{ $width }}" height="{{ $height }}">
+    <path fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.751,1.375c-.025,6.281,3.029,9.844,10,10-6.465-.025-9.672,3.441-10,10-.063-6.187-2.828-10.009-10-10C7.167,11.285,10.726,8.188,10.751,1.375Z" stroke="{{ $stroke }}"/>
+    <line x1="19.012" x2="19.012" y1="1.375" y2="5.375" fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="{{ $stroke }}"/>
+    <line x1="17.012" x2="21.012" y1="3.375" y2="3.375" fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="{{ $stroke }}"/>
+    <line x1="21.25" x2="21.25" y1="18.625" y2="22.625" fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="{{ $stroke }}"/>
+    <line x1="19.25" x2="23.25" y1="20.625" y2="20.625" fill="{{ $fill }}" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" stroke="{{ $stroke }}"/>
+</svg>
